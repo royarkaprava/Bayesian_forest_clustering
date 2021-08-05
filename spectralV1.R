4748779c6652a87729b0508717d37a6f093fa2ce
royarkaprava <34636927+royarkaprava@users.noreply.github.com>
2021-08-05 10:19:56 -0400

    Update spectralV1.R

--- spectralV1.R
+++ spectralV1.R
@@ -36,149 +36,153 @@ plot(X)
 library(emstreeR)
 library(Rcpp)
 library(RcppArmadillo)
+library(bmixture)
 
-clusteringFP <- function(X, K=20, Total_itr = 10000){
+#clusteringFP <- function(X, K=20, Total_itr = 10000){
+
+Ugamma <- function(gamma){
+  fitK <- sum(B[1, ]!=0)
+  ret <- -(p*K+2)*log(gamma)-1/gamma
   
-  Ugamma <- function(gamma){
-    fitK <- sum(B[1, ]!=0)
-    ret <- -(p*K+2)*log(gamma)-1/gamma
-    
-    return(ret)
-  }
-  
-  #initialization
-  
-  lam   <- 1 #variance for other conditional edges
-  muprvar <- 0.1 # prior variance for \mu
-  
-  p     <- ncol(X)
-  n     <- nrow(X)
-  # Xdis  <- as.matrix(exp(-dist(X)/10))
-  # #diag(Xdis) <- 1
-  # 
-  # d     <- rowSums(Xdis)
-  # LaplX <- diag(n) - diag(1/sqrt(d)) %*% Xdis %*% diag(1/sqrt(d))
-  # Lapei <- eigen(LaplX)$vectors
-  # Lapei <- Lapei[, n:(n-K+1)]
-  # sepcl <- kmeans(Lapei, centers = K)
-  
-  mu    <- c(median(X[, 1]), median(X[, 2]))
-  Xmu   <- rbind(mu, X)
-  
-  muprmn  <- mu
-  
-  d    <- data.frame(Xmu)
-  out  <- ComputeMST(d)
-  
-  B <- matrix(0, n+1, n)
-  
-  for(i in n:1){
-    B[out[i, 3], i] <- 1
-    B[out[i, 4], i] <- -1
-  }
-  
-  A <- getA(B)
-  
-  G <- igraph::graph_from_adjacency_matrix(as.matrix(A[-1,-1]),mode = "undirected")
-  pathmat <- igraph::shortest.paths(G)
-  
-  path <- NetworkToolbox::pathlengths(A[-1,-1]) #shortest path length
-  
-  clsno <- which(B[1, ] != 0)
-  l <- 1
-  clslb <- rep(0, n)
-  for(k in clsno){
-    temp <- which(B[-1, k] != 0)
-    indcompo <- which(pathmat[temp, ] < Inf)
-    
-    clslb[indcompo] <- l
-    l <- l + 1
-  }
-  edges <- out[n:1, 3:4]
- 
-  Total_itr <- 5000
-  Ap <- matrix(0, n+1, n+1)
-  Bp <- matrix(0, n+1, n)
-  B2inv <- solve(crossprod(B))
-  
-  itr = 0
-  
-  B0 =B
-  A0=A
-  
-  classMat <- matrix(0, n, K)
-  
-  classMat[cbind(1:n, clslb[1:n])] <- rep(1, n)
-  
-  clsmem <- rep(0, K)
-  for(k in 1:K){
-    clsmem[k] <- length(which(clslb==k))
-  }
-  wl <- rep(1/K, K)
-  
-  d <- rep(0, p)
-  
-  for(i in 1:p){
-    d[i] <- max(max(abs(X[,i]))-mean(X[,i]), mean(X[,i])-min(abs(X[,i])))
-  }
-  
-  gamma <- 1.5
-  
-  gamsd <- 1e-2
-  argam <- 0
-  
-  while(itr < Total_itr){
-    itr <- itr + 1
-    
-    #update B
-    #beta = rep(0, 401)
-    #j=0; k=0
-    P=1
-    N <- rep(1,200)
-    b <- rep(1, 401)
-    
-    #update B
-    BupC(B, B2inv, clslb, clsmem, b, wl, lam, 2^p*gamma^p*prod(d), Xmu)
-    A <- getA(B)
-    
-    ####Update lam
-    ind0 <- which(B[1, ]!=0)
-    sum1 <- 0
-    for(i in 1:p){
-      sum1 <- sum1 + sum((colSums(B[-1,-ind0]*X[, i]))^2) 
-    }
-    ap = 0.1 + length(n)/2 - length(ind0) / 2
-    bp = 0.1 + sum1/2
-    
-    lam = 1/rgamma(1, ap, bp)
-    
-    #update gamma
-    gammac <- gamma + rnorm(1, sd = gamsd)
-    gammac <- max(1, gammac)
-    
-    D    <- Ugamma(gammac) - Ugamma(gamma)
-    if(is.nan(D)){D=-Inf}
-    if(is.na(D)){D=-Inf}
-    
-    if(D > log(runif(1))){
-      argam <- argam + 1
-      gamma <- gammac
-    }
-    
-    
-    if(itr > 2000){
-      Ap <- Ap+A 
-      Bp <- Bp + B
-    }
-    
-    if(itr %% 100 == 0){
-      ar <- argam/ itr
-      if(ar<.20){sdgam <- sdgam * (.5)}
-      if(ar>.40){sdgam <- sdgam * (2)}
-    }
-    #update B2inv
-    print(itr)
-    #print(tau)
-  }
+  return(ret)
 }
 
+#initialization
+
+lam   <- 1 #variance for other conditional edges
+muprvar <- 0.1 # prior variance for \mu
+
+p     <- ncol(X)
+n     <- nrow(X)
+# Xdis  <- as.matrix(exp(-dist(X)/10))
+# #diag(Xdis) <- 1
+# 
+# d     <- rowSums(Xdis)
+# LaplX <- diag(n) - diag(1/sqrt(d)) %*% Xdis %*% diag(1/sqrt(d))
+# Lapei <- eigen(LaplX)$vectors
+# Lapei <- Lapei[, n:(n-K+1)]
+# sepcl <- kmeans(Lapei, centers = K)
+
+mu    <- c(median(X[, 1]), median(X[, 2]))
+Xmu   <- rbind(mu, X)
+
+muprmn  <- mu
+
+d    <- data.frame(Xmu)
+out  <- ComputeMST(d)
+
+B <- matrix(0, n+1, n)
+
+for(i in n:1){
+  B[out[i, 3], i] <- 1
+  B[out[i, 4], i] <- -1
+}
+
+A <- getA(B)
+
+G <- igraph::graph_from_adjacency_matrix(as.matrix(A[-1,-1]),mode = "undirected")
+pathmat <- igraph::shortest.paths(G)
+
+path <- NetworkToolbox::pathlengths(A[-1,-1]) #shortest path length
+
+clsno <- which(B[1, ] != 0)
+l <- 1
+clslb <- rep(0, n)
+for(k in clsno){
+  temp <- which(B[-1, k] != 0)
+  indcompo <- which(pathmat[temp, ] < Inf)
+  
+  clslb[indcompo] <- l
+  l <- l + 1
+}
+edges <- out[n:1, 3:4]
+
+Total_itr <- 5000
+Ap <- matrix(0, n+1, n+1)
+Bp <- matrix(0, n+1, n)
+B2inv <- solve(crossprod(B))
+
+itr = 0
+
+B0 =B
+A0=A
+
+classMat <- matrix(0, n, K)
+
+classMat[cbind(1:n, clslb[1:n])] <- rep(1, n)
+
+clsmem <- rep(0, K)
+for(k in 1:K){
+  clsmem[k] <- length(which(clslb==k))
+}
+alpha = 1/K
+wl <- rep(alpha, K)
+
+d <- rep(0, p)
+
+for(i in 1:p){
+  d[i] <- max(max(abs(X[,i]))-mean(X[,i]), mean(X[,i])-min(abs(X[,i])))
+}
+
+gamma <- 1.5
+
+gamsd <- 1e-2
+argam <- 0
+
+while(itr < Total_itr){
+  itr <- itr + 1
+  
+  #update B
+  #beta = rep(0, 401)
+  #j=0; k=0
+  P=1
+  N <- rep(1,200)
+  b <- rep(1, 401)
+  
+  #update B
+  BupC(B, B2inv, clslb, clsmem, b, wl, lam, 2^p*gamma^p*prod(d), Xmu)
+  A <- getA(B)
+  
+  ####Update lam
+  ind0 <- which(B[1, ]!=0)
+  sum1 <- 0
+  for(i in 1:p){
+    sum1 <- sum1 + sum((colSums(B[-1,-ind0]*X[, i]))^2) 
+  }
+  ap = 0.1 + length(n)/2 - length(ind0) / 2
+  bp = 0.1 + sum1/2
+  
+  lam = 1/rgamma(1, ap, bp)
+  
+  #update wl
+  wl <- rdirichlet(1, rep(alpha, K) + clsmem)
+  
+  #update gamma
+  gammac <- gamma + rnorm(1, sd = gamsd)
+  gammac <- max(1, gammac)
+  
+  D    <- Ugamma(gammac) - Ugamma(gamma)
+  if(is.nan(D)){D=-Inf}
+  if(is.na(D)){D=-Inf}
+  
+  if(D > log(runif(1))){
+    argam <- argam + 1
+    gamma <- gammac
+  }
+  
+  
+  if(itr > 2000){
+    Ap <- Ap+A 
+    Bp <- Bp + B
+  }
+  
+  if(itr %% 100 == 0){
+    ar <- argam/ itr
+    if(ar<.20){sdgam <- sdgam * (.5)}
+    if(ar>.40){sdgam <- sdgam * (2)}
+  }
+  #update B2inv
+  print(itr)
+  #print(tau)
+}
+#}
