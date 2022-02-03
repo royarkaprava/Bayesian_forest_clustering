14a1780818939342c5f414c5f81d7637733aa11b
royarkaprava <34636927+royarkaprava@users.noreply.github.com>
2022-02-02 20:28:27 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+This is the code of fitting forest model to cluster data generated near two manifolds.
+
+The main program is in "forestProcessV2.R", and a simulation is in "simulation.R".
+
+The code is maintained and with updates provided at the following website:
+
+https://github.com/royarkaprava/Bayesian_forest_clustering
--- simulation.R
+++ simulation.R
@@ -1,73 +1,34 @@
-if(Sys.info()["sysname"] %in% c("Linux","Darwin") ){
-  setwd("~/git/Spectral-clustering/")
-}
+require("clusterSim")
 
-if(Sys.info()["sysname"] %in% c("Windows") ){
-  setwd("\\\\file.phhp.ufl.edu\\home\\ark007\\My Documents\\GitHub\\Spectral-clustering")
-}
+n= 150
+data<- shapes.two.moon(numObjects = n/2)
 
+X<- data$data
+label<- data$clusters
 
+X<- X+ rnorm(n*2, sd=0.05)
 
+plot(X,col=label)
 
-Rcpp::sourceCpp('updateTree.cpp')
+# standardize each col of X
+X<- apply(X, 2,  function(x) (x-mean(x))/sd(x))
 
-data <- read.csv("./data.txt", header = F)
+source("forestProcessV2.R")
 
-loc <- as.matrix(data[, 1:2])
-label <- data$V3
-label[which(label=="Class 1")] <- 1
-label[which(label=="Class 2")] <- 2
-label <- as.numeric(label)
-label
+start_time <- Sys.time()
+fit <- clusteringFP(X, Total_itr = 200, burn=100)
+end_time <- Sys.time()
+end_time - start_time
 
-ind1 <- which(label==1)
-ind2 <- which(label==2)
-idx <- c(sample(ind1,100), sample(ind2, 100))
+barplot(table(sapply(fit$C,function(x) length(unique(x))))/length(fit$C))
 
-locR <- loc[idx, ]
-label_set = label[idx]
-X    <- locR
+P<- getCoAssignmentProb(fit$C)
 
-######################################################################################
+library(sommer)
+image(P,col=jet.colors(100), zlim=c(0,1))
 
-#I am initializing using ComputeMST(), there may be other ways too.
+require("kernlab")
 
-a0 =  1
-b0 =  1E-3
+posteriorMode = specc(P,centers=2)
 
-# source('forestProcessDirichlet.R')
-# fit <- clusteringFP(X,alpha0 =  0.001, a0= a0, b0 = b0, K= 20, Total_itr = 1000, burn=500, gamma=1000)
-
-source('forestProcessQuasiBernoulli.R')
-fit <- clusteringFP(X,p_b =  0.1, a0= a0, b0 = b0, K= 10, Total_itr = 1000, burn=500, gamma=1000,random_scan_n = 0)
-
-
-ts.plot(fit$sig_ls)
-acf(fit$sig_ls,lag.max = 40)
-
-min(dist(X)[dist(X)>0])
-
-rowMeans(fit$clssize_ls) #estimated class sizes
-#fit$estiadja #estimated adjacency
-rowMeans(fit$stickbrkwts) #Estimated components for stick breaking
-
-
-barplot(table(apply(fit$clslb_ls,2, function(x)length(unique(x)))))
-
-image(fit$estiadja,col = topo.colors(100, rev=F))
-
-plot(fit$estiadja[1,])
-# # fit$estiadja
-# 
-getmode <- function(v) {
-  uniqv <- unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
-}
-
-modelabel <- apply(fit$clslb_ls, 1, getmode)
-
-plot(X[,1],X[,2], col = modelabel)
-
-plot(X[,1],X[,2], col = fit$clslb_ls[,100])
-plot(X[,1],X[,2], col = fit$clslb_ls[,500])
-plot(X[,1],X[,2], col = fit$clslb_ls[,300])
+plot(X,col= as.integer(posteriorMode))
